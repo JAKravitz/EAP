0ec8909cc8fcdef044c90920fac60026d839ebd1
jakravit <jakravit@forza2.ndc.nasa.gov>
2022-01-24 10:22:35 -0600

    updates

--- split.py
+++ split.py
@@ -7,8 +7,6 @@ Created on Thu Jan 20 15:43:31 2022
 """
 #%%
 import pickle 
-with open('/Users/jakravit/git/EAP_phyto_optics.p', 'rb') as fp:
-    data0 = pickle.load(fp)
 
 with open('/Users/jakravit/git/EAP_phyto_optics1.p', 'rb') as fp:
     data1 = pickle.load(fp)
@@ -25,6 +23,15 @@ with open('/Users/jakravit/git/EAP_phyto_optics4.p', 'rb') as fp:
 with open('/Users/jakravit/git/EAP_phyto_optics5.p', 'rb') as fp:
     data5 = pickle.load(fp)
 
+with open('/Users/jakravit/git/EAP_phyto_optics6.p', 'rb') as fp:
+    data6 = pickle.load(fp)
+
+with open('/Users/jakravit/git/EAP_phyto_optics7.p', 'rb') as fp:
+    data7 = pickle.load(fp)
+
+with open('/Users/jakravit/git/EAP_phyto_optics8.p', 'rb') as fp:
+    data8 = pickle.load(fp)
+
 #%%
 def keys(d):
     sp = []
@@ -35,38 +42,57 @@ def keys(d):
             sp.append(ii)
     return sp
 
-sp = keys(data0)
-print('\n')
-print (len(sp))
-# keys(data4)
-
-# data1['Diatoms'].update(data2['Diatoms'])
-
-# keys(data1)
-
 #%%
+
+data0 = {'Green_algae': {},
+         'Cryptophytes': {},
+         'Diatoms': {},
+         'Dinoflagellates': {},
+         'Heterokonts': {},
+         'Haptophytes': {},
+         'Cyano_blue': {},
+         'Cyano_red': {},
+         'Rhodophytes': {}
+         }
+
 classes = ['Green_algae', 'Cryptophytes', 'Diatoms', 'Dinoflagellates',
-           'Heterokonts', 'Haptophytes', 'Cyano_blue', 'Cyano_red', 'Rhodophytes']
-dlist = [data1, data2, data3, data4, data5]
+            'Heterokonts', 'Haptophytes', 'Cyano_blue', 'Cyano_red', 'Rhodophytes']
+
+# for cl in classes:
+    
+#     with open('/Users/jakravit/EAP_phytoplankton_dataset/{}.p'.format(cl), 'rb') as fp:
+#         pickle.dump(data0[cl], fp)
+
+dlist = [data1, data2, data3, data4, data5, data6, data7, data8]
 
 for data in dlist:
     for cl in classes:
         data0[cl].update(data[cl])
 
-#%%
-import pandas as pd
-pdata = pd.read_csv('/Users/jakravit/git/EAP/phyto_data.csv')
-sp2 = list(pdata.Species.values)
-
-s = set(sp) - set(sp2)
-#%%
-with open('/Users/jakravit/git/EAP_phyto_optics.p', 'wb') as fp:
-    pickle.dump(data0,fp)
+sp = keys(data0)
+print('\n')
+print (len(sp))
 
 #%%
-import json
-with open('/Users/jakravit/Desktop/data1.json','w') as f:
-    json.dump(data1,f)
+# with open('/Users/jakravit/git/EAP_phyto_optics_final.p', 'wb') as fp:
+#     pickle.dump(data0,fp)
+
+for cl in classes:
+    
+    # with open('/Users/jakravit/EAP_phytoplankton_dataset/{}.p'.format(cl), 'wb') as fp:
+    #     pickle.dump(data0[cl], fp)
+    
+    with open('/Users/jakravit/EAP_phytoplankton_dataset/{}.p'.format(cl), 'wb') as fp:
+        pickle.dump(data0[cl], fp)
+
+
+#%%
+# import pandas as pd
+# pdata = pd.read_csv('/Users/jakravit/git/EAP/phyto_data.csv')
+# sp2 = list(pdata.Species.values)
+
+# s = set(sp) - set(sp2)
+#%%
 
 
 
